725c64da5389a69da80a319889a616ce9dbe4764
Dell <Dell@BLENDI>
2022-12-23 23:40:52 +0100

    Add project files.

---
+++ StudentManagementSystem/Controllers/LoginController.cs
@@ -0,0 +1,89 @@
+﻿using StudentManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StudentManagementSystem.Models;
+
+namespace WebApplication44.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        StudentManagementSystemEntities db = new StudentManagementSystemEntities();
+
+
+        //
+        // GET: /Login/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(user objchk)
+        {
+
+
+            if (ModelState.IsValid)
+            {
+                using (StudentManagementSystemEntities db = new StudentManagementSystemEntities())
+                {
+
+                    var obj = db.users.Where(a => a.username.Equals(objchk.username) && a.password.Equals(objchk.password)).FirstOrDefault();
+
+                    if (obj != null)
+                    {
+                        Session["UserID"] = obj.id.ToString();
+                        Session["UserName"] = obj.username.ToString();
+                        return RedirectToAction("Index", "Home");
+
+
+                    }
+
+                    else
+                    {
+
+                        ModelState.AddModelError("", "The UserName or Password Incorrect");
+
+
+
+                    }
+
+
+                }
+
+
+            }
+
+            return View(objchk);
+        }
+
+
+        public ActionResult Logout()
+        {
+
+            Session.Clear();
+            return RedirectToAction("Index", "Login");
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
